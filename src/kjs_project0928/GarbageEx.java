7d7b4f51390041fe4f4a39f5c8b427846683547d
No.1 <No.1@192.168.1.102>
2022-09-28 16:09:12 +0900

    This is commit!

---
+++ src/kjs_project0928/GarbageEx.java
@@ -0,0 +1,22 @@
+package kjs_project0928;
+
+public class GarbageEx {
+
+	public static void main(String[] args) {
+		String a = new String("Good");
+		String b = new String("Bad");
+		String c = new String("Normal");
+		String d, e;
+		a = null;
+		d = c;
+		c = null;
+		
+		
+		
+	
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
